c806411c7d3a25bce4ea3078b54fc4edb09a0e9a
HarshaVardhan <athaluri.harsha@gmail.com>
2022-02-16 10:11:36 +0530

    inputs demo

---
+++ inputs/src/app/democomponent.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector:'demo-comp',
+    templateUrl:'./demo.html'
+})
+export class DemoComponent{
+
+    @Input()
+    temp:any='';
+
+}
